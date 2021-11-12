633b2d061685ff45802304cb71d4e2a6bd18cb3e
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2021-11-12 20:05:58 +0100

    boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './www/index.html',
+    './src/ts/**/*.ts',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
